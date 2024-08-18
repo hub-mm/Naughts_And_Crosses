d8c50d8f5f501ec2946dc54e6157c5339f822785
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-18 14:30:41 +0100

    Add ability to colorize symbol. Add game logic: Place symbol and check if board is full.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 # board.rb
 # frozen_string_literal: true
 
+require 'colorize'
+
 # Board setup.
 class Board
   attr_reader :cells
@@ -11,15 +13,44 @@ class Board
 
   # rubocop: disable Metrics/AbcSize
   def display
-    puts "\n #{cells[0]} | #{cells[1]} | #{cells[2]}"
+    puts "\n #{colorize(cells[0])} | #{colorize(cells[1])} | #{colorize(cells[2])}"
     puts '---+---+---'
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]}"
+    puts " #{colorize(cells[3])} | #{colorize(cells[4])} | #{colorize(cells[5])}"
     puts '---+---+---'
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} \n\n"
+    puts " #{colorize(cells[6])} | #{colorize(cells[7])} | #{colorize(cells[8])} \n\n"
   end
   # rubocop: enable Metrics/AbcSize
 
   def update_board(number, symbol)
     @cells[number - 1] = symbol
   end
+
+  def place_symbol(number)
+    if @cells[number].is_a?(Integer)
+      update_board
+    else
+      'This cell is already taken, choose another spot.'
+    end
+  end
+
+  def full_board?
+    @cells.none? { |cell| cell.is_a?(Integer) }
+    puts 'That\'s a draw!'
+  end
+
+  private
+
+  def colorize(cell)
+    case cell
+    when 'X'
+      cell.colorize(:red)
+    when 'O'
+      cell.colorize(:blue)
+    else
+      cell.to_s
+    end
+  end
 end
+
+board = Board.new
+puts board.display
