8966fb99ab571930cea4524ffa5fbaa2cb848852
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-18 14:56:10 +0100

    Setup player class.

--- lib/players.rb
+++ lib/players.rb
@@ -1,1 +1,13 @@
 # players.rb
+# frozen_string_literal: true
+
+# Create players and assign symbol to players.
+class Players
+  attr_accessor :name
+  attr_reader :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
