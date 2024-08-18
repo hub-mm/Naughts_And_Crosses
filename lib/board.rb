5d3171049124d9debaf8cda722646a22e8457787
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-18 12:41:07 +0100

    Make board set up using array and puts. Board cells shown with number 1-9.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,24 @@
 # board.rb
+# frozen_string_literal: true
+
+# Board setup.
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  # rubocop: disable Metrics/AbcSize
+  def display
+    puts "\n #{cells[0]} | #{cells[1]} | #{cells[2]}"
+    puts '---+---+---'
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]}"
+    puts '---+---+---'
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} \n\n"
+  end
+  # rubocop: enable Metrics/AbcSize
+end
+
+game = Board.new
+puts game.display
